a29d8cb9009b70449de97a0ce49d1a5badd7b3e1
新島　駿 <niijima131229@docomo.ne.jp>
2022-02-11 21:10:52 +0900

    #3 人格の振り返り機能を実装

--- project/public/js/reflectionSheet.js
+++ project/public/js/reflectionSheet.js
@@ -4,6 +4,7 @@ var __webpack_exports__ = {};
   !*** ./resources/js/reflectionSheet.js ***!
   \*****************************************/
 $(function () {
+  // -----------------------オンライン教材の送信------------------------
   var checkbox = $('.checkbox');
   checkbox.on('click', function () {
     var $this = $(this);
@@ -35,7 +36,47 @@ $(function () {
       console.log(xhr);
     });
     return false;
-  });
+  }); // -----------------------人格の振り返りの送信------------------------
+
+  var personality_reflection = $('.personality_reflection');
+  personality_reflection.on('change', function () {
+    var $this = $(this);
+    var week = $this.data('week');
+    var reflection_step = $this.data('reflection_step');
+    var detail = $this.val();
+    $.ajax({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      },
+      url: '/reflection/personality_reflection',
+      //routeの記述
+      type: 'POST',
+      //受け取り方法の記述（GETもある）
+      data: {
+        week: week,
+        reflection_step: reflection_step,
+        detail: detail
+      }
+    }) // Ajaxリクエストが成功した場合
+    // .done(function () {})
+    // Ajaxリクエストが失敗した場合
+    .fail(function (data, xhr, err) {
+      //ここの処理はエラーが出た時にエラー内容をわかるようにしておく。
+      //とりあえず下記のように記述しておけばエラー内容が詳しくわかります。笑
+      console.log(err);
+      console.log(xhr);
+    });
+    return false;
+  }); // --------------------------DOMのサイズ調整---------------------------------------
+
+  function resizeSheetTitle() {
+    var teaching_material_height = $('.teaching_material').height();
+    $('#teaching_material_title').height(teaching_material_height);
+    var personality_reflection_step_height = $('.personality_reflection_step').height();
+    $('.personality_reflection_step_title').height(personality_reflection_step_height);
+  }
+
+  $(window).resize(resizeSheetTitle());
 });
 /******/ })()
 ;
--- project/routes/web.php
+++ project/routes/web.php
@@ -22,3 +22,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/reflection', 'GeneralMemberController@index')->middleware('auth')->name('general_index');
 
 Route::post('/reflection/check', 'GeneralMemberController@reflection_check')->middleware('auth')->name('reflection_check');
+Route::post('/reflection/personality_reflection', 'GeneralMemberController@personality_reflection_post')->middleware('auth')->name('personality_reflection');
